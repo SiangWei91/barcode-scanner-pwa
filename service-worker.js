279b3d47b141901554f8a4939e4510de8d5a063f
SiangWei91 <siangwei91@gmail.com>
2024-09-24 15:45:56 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Install the service worker and cache files
-const CACHE_NAME = 'barcode-scanner-cache-v2';
+const CACHE_NAME = 'barcode-scanner-cache-v3';
 const urlsToCache = [
   '/barcode-scanner-pwa/',
   '/barcode-scanner-pwa/index.html',
@@ -21,44 +21,24 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('productList.json')) {
-    // For productList.json, always fetch from network and update cache
-    event.respondWith(
-      fetch(event.request).then(function(response) {
-        // Clone the response because we're going to use it twice
-        const responseToCache = response.clone();
-        
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-
-        return response;
-      }).catch(function() {
-        // If network fetch fails, try to get it from cache
-        return caches.match(event.request);
-      })
-    );
+    // For productList.json, always go to network
+    event.respondWith(fetch(event.request));
   } else {
-    // For all other requests, try the cache first, then network
     event.respondWith(
       caches.match(event.request)
         .then(function(response) {
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
+          return response || fetch(event.request);
         })
     );
   }
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
