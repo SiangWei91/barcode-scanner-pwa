1febdc9fc971ea10770d03009279feb05278923f
SiangWei91 <siangwei91@gmail.com>
2024-09-26 10:09:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/ Updated product list
+// Updated product list
 const productList = {
   "": { itemCode: "49101", name: "FISH CAKE (L)", packingSize: "20'S" },
   "": { itemCode: "49102", name: "FISH CAKE (M)", packingSize: "20'S" },
