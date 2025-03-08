2fec3aa691a1274c7e809bfc753f13e111759400
SiangWei91 <siangwei91@gmail.com>
2025-03-08 14:42:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barcode-scanner-cache-v4';
+const CACHE_NAME = 'barcode-scanner-cache-v4.1';
 const urlsToCache = [
   '/barcode-scanner-pwa/',
   '/barcode-scanner-pwa/index.html',
